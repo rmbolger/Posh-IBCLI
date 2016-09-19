0a90f74ffea2a5c970ce7775dba89d794bbd2bec
Ryan Bolger <rbolger@directv.com>
2016-09-19 14:46:55 -0700

    Added Invoke-IBCLISetMembership and new optional param AlternatePrompt to Invoke-IBCLICommand

--- Posh-IBCLI/Public/Invoke-IBCLICommand.ps1
+++ Posh-IBCLI/Public/Invoke-IBCLICommand.ps1
@@ -23,13 +23,20 @@ function Invoke-IBCLICommand
             HelpMessage='Enter the seconds to wait for the command to complete.'
         )]
         [int]
-        $TimeoutSeconds=10
+        $TimeoutSeconds=10,
+        [Parameter(
+            Position=3,
+            HelpMessage="Enter a prompt to wait for in the output."
+        )]
+        [string]
+        $AlternatePrompt
     )
 
     # Create a regex that will match the different types of prompts that the
-    # CLI would be waiting for input on. The standard prompt waiting for a new
-    # command is 'Infoblox > '. The other type is when a command is requesting
-    # input or confirmation such as:
+    # CLI would be waiting for input on. The default prompt waiting for a new
+    # command is 'Infoblox > ' unless the default prompt has been changed with
+    # 'set prompt' in which case it still ends with ' > '. The other type of
+    # prompt is when a command is requesting input or confirmation such as:
     #
     # 'Enter Grid Name [Default Infoblox]: '
     # 'Are you sure? (y or n): '
@@ -42,12 +49,15 @@ function Invoke-IBCLICommand
 
     # collect the output while waiting for a known prompt
     $startTime = Get-Date
-    $output = ''
-    while (!($output -match $promptRegex) -and ((Get-Date) - $startTime).TotalSeconds -lt $TimeoutSeconds)
-    {
-        Start-Sleep -Seconds 1
+    do {
+        Start-Sleep -Milliseconds 500
         $output += $ShellStream.Read()
+        if ($output -match $promptRegex) { break }
+        if (![String]::IsNullOrWhiteSpace($AlternatePrompt) -and $output.EndsWith($AlternatePrompt)) { break }
     }
+    while (((Get-Date) - $startTime).TotalSeconds -lt $TimeoutSeconds)
+
+    # warn if we didn't get a recognized prompt
     if (((Get-Date) - $startTime).TotalSeconds -ge $TimeoutSeconds) {
         Write-Warning "Timed out waiting for prompt."
     }
@@ -61,13 +71,15 @@ function Invoke-IBCLICommand
     if ($lines.Count -gt 2) {
         # return all but the command echo
         return $lines[1..($lines.Count-1)]
-    } elseif ($lines.Count -eq 2) {
+    }
+    elseif ($lines.Count -eq 2) {
         # return just the prompt
         # but make sure the return value is still a single-valued array
         # rather than a standalone string due to powershell unrolling
         # https://blogs.msdn.microsoft.com/powershell/2007/01/23/array-literals-in-powershell/
         return ,@($lines[1])
-    } else {
+    }
+    else {
         # just return the single line prompt
         # and jump through the same hoops to make sure it's an array
         return ,@($lines)
@@ -90,6 +102,9 @@ function Invoke-IBCLICommand
 
     .PARAMETER TimeoutSeconds
         The number of seconds (Default: 10) to wait for the command output to finish if a known prompt isn't recognized. If the timeout is reached, the cmdlet will return whatever output it has read up until that point.
+
+    .PARAMETER AlternatePrompt
+        An alternate prompt to wait for when processing the command output. If the read output contains this value at the end of the string, it will consider the command complete and return the output. This is useful when you know in advance that a command will produce a non-standard prompt.
 
     .OUTPUTS
         System.String[]. Invoke-IBCLICommand returns a string array that contains each non-empty line of output from the command sent. The returned lines have leading and trailing whitespace trimmed.
---
+++ Posh-IBCLI/Public/Invoke-IBCLISetMembership.ps1
@@ -0,0 +1,186 @@
+function Invoke-IBCLISetMembership
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            ParameterSetName='NewStream',
+            Mandatory=$true,
+            Position=0,
+            HelpMessage='Enter the Hostname or IP Address of an Infoblox appliance.'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName,
+        [Parameter(
+            ParameterSetName='ExistingStream',
+            Mandatory=$true,
+            Position=0,
+            HelpMessage='Enter the ShellStream object returned by Connect-IBCLI.'
+        )]
+        [ValidateNotNull()]
+        [Renci.SshNet.ShellStream]
+        $ShellStream,
+        [Parameter(
+            ParameterSetName='NewStream',
+            Mandatory=$true,
+            Position=1,
+            HelpMessage='Enter the credentials for the appliance.'
+        )]
+        [PSCredential]
+        $Credential,
+        [Parameter(
+            ParameterSetName='NewStream',
+            Mandatory=$true,
+            Position=2,
+            HelpMessage='Enter the Hostname or IP Address of the grid master.'
+        )]
+        [Parameter(
+            ParameterSetName='ExistingStream',
+            Mandatory=$true,
+            Position=1,
+            HelpMessage='Enter the Hostname or IP Address of the grid master.'
+        )]
+        [string]
+        $GridMaster,
+        [Parameter(
+            ParameterSetName='NewStream',
+            Mandatory=$true,
+            Position=3,
+            HelpMessage='Enter the name of the grid.'
+        )]
+        [Parameter(
+            ParameterSetName='ExistingStream',
+            Mandatory=$true,
+            Position=2,
+            HelpMessage='Enter the name of the grid.'
+        )]
+        [string]
+        $GridName,
+        [Parameter(
+            ParameterSetName='NewStream',
+            Mandatory=$true,
+            Position=4,
+            HelpMessage='Enter grid shared secret.'
+        )]
+        [Parameter(
+            ParameterSetName='ExistingStream',
+            Mandatory=$true,
+            Position=3,
+            HelpMessage='Enter grid shared secret.'
+        )]
+        [string]
+        $GridSecret
+    )
+
+    if ($PSCmdlet.ParameterSetName -eq 'NewStream') {
+        $ShellStream = Connect-IBCLI $ComputerName $Credential -ErrorAction Stop
+    }
+    Write-Verbose "Joining $($ShellStream.Session.ConnectionInfo.Host) to $GridName grid on master $GridMaster."
+
+    try {
+
+        # call set membership
+        $output = Invoke-IBCLICommand 'set membership' $ShellStream
+
+        # enter grid master
+        if ($output[-1] -ne 'Enter new Grid Master VIP:') {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+        $output = Invoke-IBCLICommand $GridMaster $ShellStream
+
+        # enter grid name
+        if ($output[-1] -ne 'Enter Grid Name [Default Infoblox]:') {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+        $output = Invoke-IBCLICommand $GridName $ShellStream
+
+        # enter grid secret
+        if ($output[-1] -ne 'Enter Grid Shared Secret:') {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+        $output = Invoke-IBCLICommand $GridSecret $ShellStream
+
+        # confirmation 1
+        if ($output[-1] -ne 'Is this correct? (y or n):') {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+        $output = Invoke-IBCLICommand 'y' $ShellStream
+
+        # confirmation 2
+        if ($output[-1] -ne 'Are you sure? (y or n):') {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+        $output = Invoke-IBCLICommand 'y' $ShellStream 2 "until it has been configured on the grid master.`r`n"
+
+        if ($output[-1] -eq 'until it has been configured on the grid master.') {
+            Write-Verbose "Join complete. Member restarting."
+            return $true
+        } else {
+            $output | %{ Write-Verbose $_ }
+            throw "Unexpected output during 'set membership'"
+        }
+
+    } finally {
+        # disconnect if we initiated the connection here
+        if ($PSCmdlet.ParameterSetName -eq 'NewStream') {
+            Disconnect-IBCLI $ShellStream
+        }
+    }
+
+
+
+    <#
+    .SYNOPSIS
+        Join an Infoblox appliance to a grid.
+
+    .DESCRIPTION
+        Runs the 'set membership' command and answers the follow up prompts in order to join the target appliance to a grid.
+
+    .PARAMETER ComputerName
+        Hostname or IP Address of the Infoblox appliance.
+
+    .PARAMETER ShellStream
+        A Renci.SshNet.ShellStream object that was returned from Connect-IBCLI.
+
+    .PARAMETER Credential
+        Username and password for the Infoblox appliance.
+
+    .PARAMETER GridMaster
+        Hostname or IP Address of the grid master.
+
+    .PARAMETER GridName
+        The cosmetic name of the grid. 'Infoblox' is the default value on a new appliance.
+
+    .PARAMETER GridSecret
+        The grid's shared secret value used to join new members.
+
+    .OUTPUTS
+        $true if the join was successful.
+
+    .EXAMPLE
+        Invoke-IBCLISetMembership 'ns2.example.com' (get-credential) 'ns1.example.com' 'MyGrid' 'MySecret'
+
+        Joins the ns2.example.com appliance to the grid called MyGrid running on ns1.example.com.
+
+    .EXAMPLE
+        $ShellStream = Connect-IBCLI 'ns2.example.com' (Get-Credential)
+        PS C:\>Invoke-IBCLISetMembership $ShellStream 'ns1.example.com' 'MyGrid' 'MySecret'
+
+        Joins the ns2.example.com appliance to the grid called MyGrid running on ns1.example.com.
+
+    .LINK
+        Project: https://github.com/rmbolger/Posh-IBCLI
+
+    .LINK
+        Connect-IBCLI
+
+    .LINK
+        Disconnect-IBCLI
+
+    #>
+}
